6f533aaefd628794452795b2209fc0a5411bb78b
Péter Budai <budai@tresorit.com>
2019-02-04 14:35:21 +0100

    Convert to all markdown

--- plugin/reveal-viz/reveal-viz.js
+++ plugin/reveal-viz/reveal-viz.js
@@ -3,7 +3,7 @@ var RevealViz = window.RevealViz || (function() {
 
 	Reveal.addEventListener('ready', function(event) {
 		document.querySelectorAll('[data-graphviz]').forEach(function(container) {
-			viz.renderSVGElement(container.textContent)
+			viz.renderSVGElement(container.textContent, { engine: container.getAttribute('data-graphviz') || 'dot'})
 			.then(function(element) {
 				container.innerHTML = '';
 				container.appendChild(element);
